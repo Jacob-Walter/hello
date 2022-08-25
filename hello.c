63dd4f74bcfb2ad4c356f39e98d545431163da25
Jacob-Walter <111655538+Jacob-Walter@users.noreply.github.com>
2022-08-25 23:09:17 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jacob Walter
+ *
+ * Date: 2022/08/25
+ *
+ * A simple hello program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello My name is Jacob!\n");
+
+  return 0;
+}
