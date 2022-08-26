398e4f60c1638ecfa23afb2572ffcc162d67ca10
Jonny-Skelton <111836102+Jonny-Skelton@users.noreply.github.com>
2022-08-26 00:32:55 +0000

    added my name and major to the output

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Hello my name is Jacob!\n");
   printf("My Major is Computer Science");
+  printf("My name is Jonathan Skelton!\n");
+  printf("My major is Computer Science!\n");
 
   return 0;
 }
